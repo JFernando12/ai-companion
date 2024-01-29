6820c85dfbe3c63a343e5d068a8cd20a91f956df
Fernando Castrejon <fernandocastrejonh@gmail.com>
2024-01-29 08:04:40 -0600

    changes

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,13 @@
+const layout = ({
+  children
+}: {
+  children: React.ReactNode
+}) => {
+  return (
+    <div className='flex justify-center items-center h-full'>
+      {children}
+    </div>
+  );
+};
+
+export default layout;
